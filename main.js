fa758c05c1b2911d432c0156acd5705224982a05
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2024-10-01 15:00:29 +0200

    Easy add new image
    But flat

--- main.js
+++ main.js
@@ -1,39 +1,53 @@
-// Function to update rotation dynamically
-function updateRotation(x, y, z,id) {
+function createImage(imageUrl, newId, newText, imageLink) {
+    marker = document.createElement('a-marker');
+    scene = document.getElementById("scene");
+    marker.setAttribute("type", "pattern");
+    marker.setAttribute("preset", "custom");
+    marker.setAttribute("id",newId)
+    marker.setAttribute("url", imageUrl);
+    marker.setAttribute("smooth","true")
+    marker.setAttribute("smoothCount","10")
+    marker.setAttribute("smoothTolerance",".01")
+    marker.setAttribute("smoothThreshold","5")
+
+    aText = document.createElement('a-text');
+    aText.setAttribute("value",newText)
+    aText.setAttribute("rotation","270 270 90")
+    aText.setAttribute("position","0 0 1.2")
+    aText.setAttribute("align","center")
+    marker.appendChild(aText)
+
+    aImage = document.createElement('a-image');
+    aImage.setAttribute("rotation","90 180 0")
+    aImage.setAttribute("position","0 0 0")
+    aImage.setAttribute("scale","2 2")
+    aImage.setAttribute("src",imageLink)
+    marker.appendChild(aImage)
+
+    scene.appendChild(marker)
+}
+function updateRotation(x, y, z, id) {
     var pigModel = document.querySelector(`#${id}`);
     pigModel.setAttribute('rotation', `${x} ${y} ${z}`);
 }
 
-function jump(x,y,z,id) {
+function jump(x, y, z, id) {
     var picture = document.querySelector(`#${id}`);
     picture.setAttribute('position', `${x} ${y} ${z}`)
 }
 
-let height = 0
-let up = true
 setInterval(() => {
-    if (up && height < 1) {
-        height += 0.05
-    } else if (height > 0) {
-        up = false
-        height -= 0.05
-    } else {
-        up = true
-    }
-    jump(0,0,-height,"VanGogh")
 }, 30)
 
 setTimeout(()=> {
-    findMark("markerVan")
-},10)
+    createImage("Marker/chat.patt", "test", "Maleri", "Images/Monumental_Figure.jpg")
 
-function findMark(id) {
-    let mark = document.querySelector(`#${id}`)
+    let mark = document.querySelector(`#test`)
+
     mark.addEventListener("markerFound", () => {
-        let pos = getScreenPosition(mark)
         showConfetti()
     })
-}
+},10)
 
 function showConfetti() {
     confetti({
@@ -41,9 +55,4 @@ function showConfetti() {
         spread: 70,
         origin: { x: 0.5, y: 0.6 }
     });
-}
-
-function getScreenPosition(marker) {
-    let marker3DPos = marker.object3D.position
-    console.log(marker3DPos)
 }
