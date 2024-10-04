08be332b77206d8e3bb2276eca613b99ed6d630e
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2024-10-04 13:14:16 +0200

    Clean up

--- main.js
+++ main.js
@@ -5,51 +5,51 @@ let touchstartX = 0
 let touchendX = 0
 
 class ImageAR {
-    constructor(imageUrl, newId, newText, imageLink) {
-        this.textArr = newText;
-        this.textNr = 0;
-        this.createImage(imageUrl, newId, imageLink)
-        
-    }
+  constructor(imageUrl, newId, newText, imageLink) {
+    this.textArr = newText;
+    this.textNr = 0;
+    this.createImage(imageUrl, newId, imageLink)
     
-    nextText() {
-        this.textNr++
-        if (this.textNr >= this.textArr.length) this.textNr = 0;
-        this.aText.setAttribute("value",this.textArr[this.textNr])
-    }
+  }
+  
+  nextText() {
+    this.textNr++
+    if (this.textNr >= this.textArr.length) this.textNr = 0;
+    this.aText.setAttribute("value",this.textArr[this.textNr])
+  }
 
-    change(newImage) {
-        this.aImage.setAttribute("src",newImage)
-    }
+  change(newImage) {
+    this.aImage.setAttribute("src",newImage)
+  }
 
-    createImage(imageUrl, newId, imageLink) {
-        this.marker = document.createElement('a-marker');
-        this.scene = document.getElementById("scene");
-        this.marker.setAttribute("type", "pattern");
-        this.marker.setAttribute("preset", "custom");
-        this.marker.setAttribute("id",newId)
-        this.marker.setAttribute("url", imageUrl);
-        this.marker.setAttribute("smooth","true")
-        this.marker.setAttribute("smoothCount","10")
-        this.marker.setAttribute("smoothTolerance",".01")
-        this.marker.setAttribute("smoothThreshold","5")
-    
-        this.aText = document.createElement('a-text');
-        this.aText.setAttribute("value",this.textArr[this.textNr])
-        this.aText.setAttribute("rotation","270 270 90")
-        this.aText.setAttribute("position","0 0 1.2")
-        this.aText.setAttribute("align","center")
-        this.marker.appendChild(this.aText)
-    
-        this.aImage = document.createElement('a-image');
-        this.aImage.setAttribute("rotation","90 180 0")
-        this.aImage.setAttribute("position","0 0 0")
-        this.aImage.setAttribute("scale","2 2")
-        this.aImage.setAttribute("src",imageLink)
-        this.marker.appendChild(this.aImage)
-    
-        this.scene.appendChild(this.marker)
-    }
+  createImage(imageUrl, newId, imageLink) {
+    this.marker = document.createElement('a-marker');
+    this.scene = document.getElementById("scene");
+    this.marker.setAttribute("type", "pattern");
+    this.marker.setAttribute("preset", "custom");
+    this.marker.setAttribute("id",newId)
+    this.marker.setAttribute("url", imageUrl);
+    this.marker.setAttribute("smooth","true")
+    this.marker.setAttribute("smoothCount","10")
+    this.marker.setAttribute("smoothTolerance",".01")
+    this.marker.setAttribute("smoothThreshold","5")
+
+    this.aText = document.createElement('a-text');
+    this.aText.setAttribute("value",this.textArr[this.textNr])
+    this.aText.setAttribute("rotation","270 270 90")
+    this.aText.setAttribute("position","0 0 1.2")
+    this.aText.setAttribute("align","center")
+    this.marker.appendChild(this.aText)
+
+    this.aImage = document.createElement('a-image');
+    this.aImage.setAttribute("rotation","90 180 0")
+    this.aImage.setAttribute("position","0 0 0")
+    this.aImage.setAttribute("scale","2 2")
+    this.aImage.setAttribute("src",imageLink)
+    this.marker.appendChild(this.aImage)
+
+    this.scene.appendChild(this.marker)
+  }
 }
 
 setInterval(() => {
@@ -57,25 +57,27 @@ setInterval(() => {
 }, 10)
 
 function updateText(event) {
-    image1.nextText();
-    image2.nextText();
-    mouseX = event.clientX;
-    mouseY = event.clientY;
+  image1.nextText();
+  image2.nextText();
+  mouseX = event.clientX;
+  mouseY = event.clientY;
 
 }
 
 function changeImage(event) {
-    if (10 < event.clientX - mouseX || -10 > (event.clientX - mouseX)) {
-        image1.change("Images/Cat.jpg")
-        image2.change("Images/Monumental_Figure.jpg")
-    }
+  if (10 < event.clientX - mouseX || -10 > (event.clientX - mouseX)) {
+    image1.change("Images/Cat.jpg")
+    image2.change("Images/Monumental_Figure.jpg")
+  }
 }
 
 
     
 function checkDirection() {
+  if (touchendX < touchstartX || touchendX > touchstartX) {
     image1.change("Images/Cat.jpg")
     image2.change("Images/Monumental_Figure.jpg")
+  }
 }
 
 document.addEventListener('touchstart', e => {
@@ -88,20 +90,20 @@ document.addEventListener('touchend', e => {
 })
 
 setTimeout(()=> {
-    image1 = new ImageAR("Marker/chat.patt", "Gris", ["Dette er en gris", "Den er pink"], "Images/Monumental_Figure.jpg")
-    image2 = new ImageAR("Marker/pattern-marker.patt", "Cat", ["Dette er en cat", "Den er gul"], "Images/Cat.jpg")
+  image1 = new ImageAR("Marker/chat.patt", "Gris", ["Dette er en gris", "Den er pink"], "Images/Monumental_Figure.jpg")
+  image2 = new ImageAR("Marker/pattern-marker.patt", "Cat", ["Dette er en cat", "Den er gul"], "Images/Cat.jpg")
 
-    let mark = document.querySelector(`#Gris`)
+  let mark = document.querySelector(`#Gris`)
 
-    mark.addEventListener("markerFound", () => {
-        showConfetti()
-    })
-},10)
+  mark.addEventListener("markerFound", () => {
+      showConfetti()
+  })
+},1)
 
 function showConfetti() {
-    confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { x: 0.5, y: 0.6 }
-    });
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { x: 0.5, y: 0.6 }
+  });
 }
