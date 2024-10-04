f8d7a6799ff958c16f638453d275eb2b53707905
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2024-10-04 10:22:37 +0200

    Change text on click

--- main.js
+++ main.js
@@ -1,40 +1,64 @@
-function createImage(imageUrl, newId, newText, imageLink) {
-    marker = document.createElement('a-marker');
-    scene = document.getElementById("scene");
-    marker.setAttribute("type", "pattern");
-    marker.setAttribute("preset", "custom");
-    marker.setAttribute("id",newId)
-    marker.setAttribute("url", imageUrl);
-    marker.setAttribute("smooth","true")
-    marker.setAttribute("smoothCount","10")
-    marker.setAttribute("smoothTolerance",".01")
-    marker.setAttribute("smoothThreshold","5")
+class ImageAR {
+    constructor(imageUrl, newId, newText, imageLink) {
+        this.textArr = newText;
+        this.textNr = 0;
+        this.createImage(imageUrl, newId, imageLink)
+        
+    }
+    
+    nextText() {
+        this.textNr++
+        if (this.textNr >= this.textArr.length) this.textNr = 0;
+        this.aText.setAttribute("value",this.textArr[this.textNr])
+    }
 
-    aText = document.createElement('a-text');
-    aText.setAttribute("value",newText)
-    aText.setAttribute("rotation","270 270 90")
-    aText.setAttribute("position","0 0 1.2")
-    aText.setAttribute("align","center")
-    marker.appendChild(aText)
-
-    aImage = document.createElement('a-image');
-    aImage.setAttribute("rotation","90 180 0")
-    aImage.setAttribute("position","0 0 0")
-    aImage.setAttribute("scale","2 2")
-    aImage.setAttribute("src",imageLink)
-    marker.appendChild(aImage)
-
-    scene.appendChild(marker)
+    createImage(imageUrl, newId, imageLink) {
+        this.marker = document.createElement('a-marker');
+        this.scene = document.getElementById("scene");
+        this.marker.setAttribute("type", "pattern");
+        this.marker.setAttribute("preset", "custom");
+        this.marker.setAttribute("id",newId)
+        this.marker.setAttribute("url", imageUrl);
+        this.marker.setAttribute("smooth","true")
+        this.marker.setAttribute("smoothCount","10")
+        this.marker.setAttribute("smoothTolerance",".01")
+        this.marker.setAttribute("smoothThreshold","5")
+    
+        this.aText = document.createElement('a-text');
+        this.aText.setAttribute("value",this.textArr[this.textNr])
+        this.aText.setAttribute("rotation","270 270 90")
+        this.aText.setAttribute("position","0 0 1.2")
+        this.aText.setAttribute("align","center")
+        this.marker.appendChild(this.aText)
+    
+        this.aImage = document.createElement('a-image');
+        this.aImage.setAttribute("rotation","90 180 0")
+        this.aImage.setAttribute("position","0 0 0")
+        this.aImage.setAttribute("scale","2 2")
+        this.aImage.setAttribute("src",imageLink)
+        this.marker.appendChild(this.aImage)
+    
+        this.scene.appendChild(this.marker)
+    }
 }
 
 setInterval(() => {
-}, 30)
+
+}, 8000)
+
+function updateText() {
+    image1.nextText();
+    image2.nextText();
+
+}
 
 setTimeout(()=> {
-    createImage("Marker/chat.patt", "Gris", "Dette er en gris", "Images/Monumental_Figure.jpg")
-    createImage("Marker/pattern-marker.patt", "Cat", "Dette er en cat", "Images/Cat.jpg")
+    image1 = new ImageAR("Marker/chat.patt", "Gris", ["Dette er en gris", "Den er pink"], "Images/Monumental_Figure.jpg")
+    image2 = new ImageAR("Marker/pattern-marker.patt", "Cat", ["Dette er en cat", "Den er gul"], "Images/Cat.jpg")
 
-    let mark = document.querySelector(`#test`)
+    // onmousedown = image1.nextText()
+
+    let mark = document.querySelector(`#Gris`)
 
     mark.addEventListener("markerFound", () => {
         showConfetti()
